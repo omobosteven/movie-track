18738082a96fd8078cb0ace5a134a483e5f386f5
omobosteven <omobosteven@gmail.com>
2022-11-15 22:18:13 +0100

    fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "prettier"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "prettier/prettier": "error",
-  },
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint", "prettier"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"prettier/prettier": "error",
+	},
 };
